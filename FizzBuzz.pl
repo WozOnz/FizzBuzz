9f3d1b246061d7dbffcb12eca50b4c7e13bf63c1
WozOnz <woz@narutaka.org>
2015-09-24 19:23:07 +0900

    配列を使った場合

---
+++ FizzBuzz.pl
@@ -0,0 +1,11 @@
+my @f = ("Fizz");
+my @b = ("Buzz");
+foreach(1..100)
+{
+	my $msg = "";
+	if( $f[$_%3] ){ $msg = $f[$_%3]; }
+	if( $b[$_%5] ){ $msg = $b[$_%5]; }
+	if( !$msg ){ $msg = $_;}
+	print qq($msg\n);
+}
+exit;
