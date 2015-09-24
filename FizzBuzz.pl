53dd49b81c4f212c881abab4ef2ebf5a86949af4
WozOnz <woz@narutaka.org>
2015-09-24 19:32:29 +0900

    マージ修正

--- FizzBuzz.pl
+++ FizzBuzz.pl
@@ -1,15 +1,7 @@
-my @f = ("Fizz");
 my @f = qq(Fizz), @b = qq(Buzz);
 foreach(1..100)
 {
-<<<<<<< HEAD
 	my $msg = ($f[$_%3].$b[$_%5]||$_);
-=======
-	my $msg = "";
-	if( $f[$_%3] ){ $msg = $f[$_%3]; }
-	if( $b[$_%5] ){ $msg .= $b[$_%5]; }
-	if( !$msg ){ $msg = $_;}
->>>>>>> test
 	print qq($msg\n);
 }
 0;
