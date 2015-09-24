885d9c3db29933203f991351a759e9dac3ce19da
WozOnz <woz@narutaka.org>
2015-09-24 19:27:31 +0900

    ZizzBuzzと出ないバグ修正

--- FizzBuzz.pl
+++ FizzBuzz.pl
@@ -3,7 +3,7 @@ foreach(1..100)
 {
 	my $msg = "";
 	if( $f[$_%3] ){ $msg = $f[$_%3]; }
-	if( $b[$_%5] ){ $msg = $b[$_%5]; }
+	if( $b[$_%5] ){ $msg .= $b[$_%5]; }
 	if( !$msg ){ $msg = $_;}
 	print qq($msg\n);
 }
